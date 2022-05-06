64236dce5cadae8475703ff44ff67d675766776b
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-05-06 13:42:55 -0500

    updated category GET routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,19 +4,27 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  Category.findAll({})
+  Category.findAll({
+    include: {
+      model: Product,
+      as: 'products'
+    }
+  })
     .then(dbCategoryData => res.json(dbCategoryData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
-  // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   Category.findOne({
     where: {
       id: req.params.id
+    },
+    include: {
+      model: Product,
+      as: 'products'
     }
   })
     .then(dbCategoryData => {
@@ -30,7 +38,6 @@ router.get('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     })
-  // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
